0d30a31aa92ab58490b8a0bdb55677a41b05b7f0
travi01 <travi01@proton.me>
2026-03-16 23:05:15 +0700

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -13,7 +13,7 @@ class LK21Provider : MainAPI() {
 
     override suspend fun search(query: String): List<SearchResponse> {
         val link = "$mainUrl/?s=$query"
-        val document = app.get(link).deocument
+        val document = app.get(link).document
         return document.select("article.ui-grid-main").map {
             val title = it.selectFirst("h1.grid-title a")?.text() ?: ""
             val href = it.selectFirst("h1.grid-title a")?.attr("href") ?: ""
